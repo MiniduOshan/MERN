767b35b1500b72c384b168516696bec3d0429176
MiniduOshan <126508461+MiniduOshan@users.noreply.github.com>
2025-11-20 22:34:02 +0530

    commit

--- MERN/src/pages/product.jsx
+++ MERN/src/pages/product.jsx
@@ -1,20 +1,28 @@
 import React, { useEffect } from 'react'
 
 function Product() {
-  
+     
+      const [products, setProducts] = React.useState([])
+
         async function fetchProducts() {
          const data = await fetch("https://fakestoreapi.com/products")
          const products = await data.json()
-            console.log(products)
+        setProducts(products)
+        console.log(products)
       }
       useEffect(()=>{
             fetchProducts()
       }, [])
   
 return (
-    <div>
+    <div className="min-h-screen bg-gray-100 p-6">
+    <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">
+        Products
+      </h2>
+      <div className="max-w-5xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6"></div>
       <ul>
-      {products.map(product=>{
+      {products.map(product=>(
+        
         <li key={product.id}>{product.title}</li>))
       }
       </ul>
